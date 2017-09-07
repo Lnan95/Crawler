076a295b61c1697ce2abf99a17807d9618cd0e8f
Lin Sinan <464818805@qq.com>
2017-09-07 22:56:22 +0800

    v1.14

--- dd373.py
+++ dd373.py
@@ -10,7 +10,7 @@ import time
 import pandas as pd
 
 # 初始参数
-frequency = 1 # 绘图频率
+frequency = 3 # 绘图频率（绘图间隔 = frequency * sleep_time）
 sleep_time = 300 # 每300s更新一次数据
 alpha = 0.5 # 大商和散户的权重
 
@@ -61,105 +61,69 @@ while(1):
             main_price.append(float(price[i][3:8]))
         else:
             other_price.append(float(price[i][3:8]))
-    history_time.append(datetime.datetime.now())
+    history_time.append(datetime.datetime.now().strftime('%H:%M'))
     history_main_price.append(np.median(main_price))
     history_other_price.append(np.median(other_price))
-    mix = np.dot(history_main_price, alpha) + np.dot(history_other_price, 1-alpha)
 
-    print("时间: "+ datetime.datetime.now().strftime('%m-%d %H:%M'))
+    mix = np.dot(history_main_price, alpha) + np.dot(history_other_price, 1 - alpha)
+
+    print("时间: "+ datetime.datetime.now().strftime('%H:%M'))
     print("综合价格: {}".format(mix[-1]))
     print("大商价格: {}".format(history_main_price[-1]))
     print("散户价格: {}".format(history_other_price[-1])+"\n")
 
-    #画图 & 数据保存
-    if((count % frequency) ==0):
-        data = pd.DataFrame({'时间': history_time, '大商价格': history_main_price, '散户价格': history_other_price})
-        data.to_csv(datetime.datetime.now().strftime('%m%d')+'.csv') # 按日期保存
+    # 画图 & 数据保存
+    if((count % frequency) == 0):
 
+        # 数据保存
+        data = pd.DataFrame({'time': history_time, 'dashang': history_main_price, 'sanhu': history_other_price})
+        filename = datetime.datetime.now().strftime('%m%d') + '.csv'  # 按日期保存
+        if os.path.exists(filename):
+            ori_data = pd.read_csv(filename)
+            # ori_data['time'] = datetime.datetime.strptime(pd.to_datetime(ori_data['time']), "%Y-%m-%d %H:%M")
+            data = ori_data.append(data)
+        data.to_csv(filename, index=False)
+
+        history_time = data['time']
+        history_main_price = data['dashang']
+        history_other_price = data['sanhu']
+
+        length = len(history_time)
+        x_axis = np.linspace(0, length-1, length)
+        # 计算综合价格
+        mix = np.dot(history_main_price, alpha) + np.dot(history_other_price, 1 - alpha)
+
+
+        # 绘图1
         plt.figure(figsize=(20, 5))
         plt.suptitle(u"DD373冒险岛2女王镇货币价格：{}".format(datetime.datetime.now().strftime('%m-%d'))
                      , fontsize="15")
 
-        plt.plot(history_time,history_main_price,label='大商价格',color='red',linewidth=1.0,linestyle='--') #默认
-        plt.plot(history_time,history_other_price,label='散户价格',color='orange',linewidth=1.0,linestyle='--')
-        plt.plot(history_time,mix,label='综合价格',color='blue',linewidth=1.2)
+        plt.plot(x_axis,history_main_price,label='大商价格',color='red',linewidth=1.0,linestyle='--') #默认
+        plt.plot(x_axis,history_other_price,label='散户价格',color='orange',linewidth=1.0,linestyle='--')
+        plt.plot(x_axis,mix,label='综合价格',color='blue',linewidth=1.2)
         # 标注方法
-        plt.text(history_time[0], mix[0]+0.3, "1:{}w".format(format(mix[0],"0.2f")))
-        plt.text(history_time[-1], mix[-1]+0.3, "1:{}w".format(format(mix[-1],"0.2f")))
+        plt.text(x_axis[0], mix[0]+0.35, "1:{}w".format(format(mix[0],"0.2f")))
+        plt.text(x_axis[-1], mix[-1]+0.35, "1:{}w".format(format(mix[-1],"0.2f")))
         # 最大值&最小值
-        max_index = history_main_price.index(max(history_main_price))
-        plt.annotate("max: 1:{}w".format(format(mix[max_index],"0.2f")) % mix[max_index],xy=(history_time[max_index],mix[max_index]),xycoords='data',xytext=(-30,+30),textcoords='offset points',
+        max_index = mix.argmax()
+        plt.annotate("max: 1:{}w".format(format(mix[max_index],"0.2f")) % mix[max_index],xy=(x_axis[max_index],mix[max_index]),xycoords='data',xytext=(-30,+30),textcoords='offset points',
                  color='red',arrowprops=dict(arrowstyle='->',color='red',connectionstyle='arc3,rad=-0.2'))
-        min_index = history_main_price.index(min(history_main_price))
-        plt.annotate("min: 1:{}w".format(format(mix[min_index],"0.2f")) % mix[min_index],xy=(history_time[min_index],mix[min_index]),xycoords='data',xytext=(+30,-30),textcoords='offset points',
+        min_index = mix.argmin()
+        plt.annotate("min: 1:{}w".format(format(mix[min_index],"0.2f")) % mix[min_index],xy=(x_axis[min_index],mix[min_index]),xycoords='data',xytext=(+30,-30),textcoords='offset points',
                      color='green',arrowprops=dict(arrowstyle='->',color='green',connectionstyle='arc3,rad=-0.2'))
 
         plt.legend(loc='upper left')
+        plt.xticks(np.arange(length), history_time)
 
-        # 保存
+        # 保存图片
         fig = plt.gcf()
         tmp = datetime.datetime.now().strftime('%m%d')+ '.png'
         fig.savefig(tmp)
         plt.close()
 
+        history_main_price = []
+        history_other_price = []
+        history_time = []
+
     time.sleep(sleep_time)
-
-
-while(False):
-    def get_week_day(date):
-      week_day_dict = {
-        0 : '星期一',
-        1 : '星期二',
-        2 : '星期三',
-        3 : '星期四',
-        4 : '星期五',
-        5 : '星期六',
-        6 : '星期天',
-      }
-      day = date.weekday()
-      return week_day_dict[day]
-
-
-
-    history_main_price = []
-    history_other_price = []
-    history_time = []
-    while(1):
-        main_price = 0
-        other_price = 0
-        url = 'http://www.dd373.com/s/1xj2qx-wjm3vp-qmfpmj-0-0-0-tr1r70-0-0-0-0-su-0-512-0.html'
-        request_headers = {
-                'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36"
-                }
-
-        response = requests.get(url,headers=request_headers)
-
-        data = response.text
-        regex = '1元=\d\d...'
-        pa = re.compile(regex)
-        ma = re.findall(pa,data)
-        for i in range(13):
-                if i <= 2:
-                        main_price += float(ma[i][3:8])
-                else:
-                        other_price += float(ma[i][3:8])
-        history_time.append(datetime.datetime.now())
-        history_main_price.append(np.mean(main_price)/3)
-        history_other_price.append(np.mean(other_price)/10)
-
-        plt.figure(figsize=(10,5))
-        #画图
-        mix = np.dot(history_main_price,0.6)+ np.dot(history_other_price,0.4)
-        plt.plot(history_time,history_main_price,label='大商价格',color='red',linewidth=1.0,linestyle='--') #默认
-        plt.plot(history_time,history_other_price,label='散户价格',color='orange',linewidth=1.0,linestyle='--')
-        plt.plot(history_time,mix,label='综合价格',color='blue',linewidth=1.2)
-        plt.legend(loc='best')
-        fig = plt.gcf()
-        tmp = datetime.datetime.now().strftime('%m%d%H%M')+ '.png'
-        fig.savefig(tmp)
-
-        time.sleep(300)
-
-
-        for j in range(len(history_main_price)):
-            plt.text(history_main_price[j] , history_time, 'this is a sin(x)line')
